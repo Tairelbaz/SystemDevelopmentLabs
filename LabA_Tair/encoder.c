5108dde081352e465638dc53201c554a6d1e04d0
Tairelbaz <tair300556@gmail.com>
2026-03-30 20:33:26 +0300

    more changes to encoder and my_echo

--- LabA_Tair/encoder.c
+++ LabA_Tair/encoder.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned char password[] = "my_password1";
+const char password[] = "my_password1";
 FILE *infile = NULL;
 FILE *outfile = NULL;
+const char *encoding_key = "A";
+int encoding_sign = 1;
+int encoding_pos = 0;
 
-// Part 1: Simple echo encoder
 int encode(int c) {
-    return c; // For now, just return the character unchanged
+    int shift;
+    int base;
+
+    if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+        shift = encoding_key[encoding_pos] - 'A';
+        shift *= encoding_sign;
+        base = (c >= 'A' && c <= 'Z') ? 'A' : 'a';
+
+        c = base + ((c - base + shift + 26) % 26);
+
+        encoding_pos++;
+        if(encoding_key[encoding_pos] == '\0') {
+            encoding_pos = 0;
+        }
+    }
+
+    return c;
 }
 
 int main(int argc, char *argv[]) {
@@ -18,7 +36,7 @@ int main(int argc, char *argv[]) {
     outfile = stdout;
 
     // Iterate over command line arguments
-    for(int i = 1; i < argc; i++) {
+    for(int i = 0; i < argc; i++) {
         // If debug mode is on, print the argument to stderr
         if(debug) {
             fprintf(stderr, "%s\n", argv[i]);
@@ -27,8 +45,16 @@ int main(int argc, char *argv[]) {
         // Check for debug flags to change mode for NEXT arguments
         if(strcmp(argv[i], "-D") == 0) {
             debug = 0;
-        } else if(strncmp(argv[i], "+D", 2) == 0 && strcmp(argv[i] + 2, (char *)password) == 0) {
+        } else if(strncmp(argv[i], "+D", 2) == 0 && strcmp(argv[i] + 2, password) == 0) {
             debug = 1;
+        } else if(strncmp(argv[i], "+V", 2) == 0) {
+            encoding_key = argv[i] + 2;
+            encoding_sign = 1;
+            encoding_pos = 0;
+        } else if(strncmp(argv[i], "-V", 2) == 0) {
+            encoding_key = argv[i] + 2;
+            encoding_sign = -1;
+            encoding_pos = 0;
         }
     }
 
