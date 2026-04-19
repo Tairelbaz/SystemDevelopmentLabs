c5c7c0ec414ba0325f53a8e5a585dd4aeb4e1a91
offeck <offeck100@gmail.com>
2026-04-19 17:20:11 +0300

    Refactor Bubblesort implementation for improved memory management and sorting logic

--- LabB/Bubblesort.c
+++ LabB/Bubblesort.c
@@ -4,17 +4,16 @@
 void bubbleSort(int numbers[], int array_size)
 {
     int i, j;
-    int *temp;
+    int temp;
     for (i = (array_size - 1); i > 0; i--)
     {
-        for (j = 1; j < i; j++)
+        for (j = 1; j <= i; j++)
         {
             if (numbers[j - 1] > numbers[j])
             {
-                temp = (int *)malloc(sizeof(int *));
-                *temp = numbers[j - 1];
+                temp = numbers[j - 1];
                 numbers[j - 1] = numbers[j];
-                numbers[j] = *temp;
+                numbers[j] = temp;
             }
         }
     }
@@ -27,7 +26,7 @@ int main(int argc, char **argv)
     int *numbers = (int *)calloc(n, sizeof(int));
 
     printf("Original array:");
-    for (i = 0; i <= n; ++i)
+    for (i = 0; i < n; ++i)
     {
         printf(" %s", arr[i]);
         numbers[i] = atoi(arr[i]);
@@ -40,5 +39,6 @@ int main(int argc, char **argv)
     for (i = 0; i < n; ++i)
         printf(" %d", numbers[i]);
     printf("\n");
+    free(numbers);
     return 0;
 }
